7f3fcc81aa21272c75e0166c50c7bad87bd212c7
Abdoul Kader <kaderabdou895@gmail.com>
2025-01-19 15:28:43 +0100

    important

--- files/script.js
+++ files/script.js
@@ -35,6 +35,28 @@ let changeText = () =>{
 changeText();
 setInterval(changeText, 3000);
 
+const circles = document.querySelectorAll(".circle");
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 /dots;
+
+    for(let i = 0; i < dots ; i++){
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i = 0; i < percent; i++){
+        pointsMarked[i].classList.add('marked');
+    }
+});
+
+
+
+
 
 let menuLi = document.querySelectorAll('header ul li a');
 let section = document.querySelectorAll('section');
@@ -73,3 +95,25 @@ window.onscroll = () =>{
     menuIcoon.classList.add("fa-bars");
     navlist.classList.remove("open");
 }
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+const scrollbottom = document.querySelectorAll(".scroll-bottom");
+scrollbottom.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
+
+
+var mixer = mixitup('.portfolio-gallery');
